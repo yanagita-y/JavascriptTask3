f76700002403b351f74d233e47464885b434866f
yanagita-y <yosiphi@hotmail.com>
2022-02-09 22:56:00 +0900

    2022/02/09JavaScriptTask3-①修正

--- taskscript.js
+++ taskscript.js
@@ -1,26 +1,35 @@
+const alltask = [];
+
 const taskscript = (taskname) => {
-    const output = document.getElementById("output");
-    const tasks = {name: taskA, status: '作業中'};
     if(taskname !== ''){
-        const tr = document.createElement('tr');
-        output.appendChild(tr);
-        const td1 = document.createElement('td');
-        const id = output.childElementCount;
-        tr.appendChild(td1);
-        td1.textContent = id-1;
-        const td2 = document.createElement('td');
-        tr.appendChild(td2);
-        td2.textContent = taskname;
-        const td3 = document.createElement('td');
-        tr.appendChild(td3);
-        const button1 = document.createElement('button');
-        td3.appendChild(button1);
-        button1.textContent = "作業中";
-        const td4 = document.createElement('td');
-        tr.appendChild(td4);
-        const button2 = document.createElement('button');
-        td4.appendChild(button2);
-        button2.textContent = "削除";
-        document.getElementById("singletask").value = "";
+        const tasks = {name: taskname, status: '作業中'};
+        alltask.push(tasks);
+        showTasks();
     }
+}
+
+const showTasks = () => {
+    const output = document.getElementById("output");
+    output.textContent = null;
+    alltask.forEach((task, index) => {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+        const button1 = document.createElement('button');
+        const td4 = document.createElement('td');
+        const button2 = document.createElement('button');
+        document.getElementById("singletask").value = "";
+        button1.textContent = "作業中";
+        button2.textContent = "削除";
+        td1.textContent = index;
+        td2.textContent = task.name;
+        output.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        td3.appendChild(button1);
+        tr.appendChild(td4);
+        td4.appendChild(button2);
+    });
 }
