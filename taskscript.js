d70cb73beb3202c9069937774c9474b9ced518db
yanagita-y <yosiphi@hotmail.com>
2022-02-16 02:14:23 +0900

    20220216 JavaScript3-②修正

--- taskscript.js
+++ taskscript.js
@@ -13,6 +13,38 @@ const deltask = (index) =>{
     showTasks();
 }
 
+const mkdelbutton = (td4, index) =>{
+    const deleteButton = document.createElement('button');
+    deleteButton.addEventListener("click", () => {
+        deltask(index);
+    })
+    deleteButton.textContent = "削除";
+    td4.appendChild(deleteButton);
+}
+
+const mkstatbutton = (td3, status ,index) =>{
+    const statusButton = document.createElement('button');
+    statusButton.textContent = status;
+    td3.appendChild(statusButton);
+    // statusButton.addEventListener("click", () => {
+    //     statusButton.textContent = transtask(index);
+    // })
+}
+
+// const transtask = (index) =>{
+//     if(tasks[index].status === "作業中"){
+//         tasks[index].status = "完了";
+//     }else{
+//         tasks[index].status = "作業中";
+//     }
+//     return tasks[index].status;
+// }
+
+const mktaskinfo = (td1,td2,index,taskname) =>{
+    td1.textContent = index;
+    td2.textContent = taskname;
+}
+
 const showTasks = () => {
     const output = document.getElementById("output");
     output.textContent = '';
@@ -22,26 +54,14 @@ const showTasks = () => {
         const td1 = document.createElement('td');
         const td2 = document.createElement('td');
         const td3 = document.createElement('td');
-        const button1 = document.createElement('button');
         const td4 = document.createElement('td');
-        const button2 = document.createElement('button');
-        button2.value = index;
-//        button2.setAttribute('onclick', `deltask(${index})`);
-        // ここで「削除」をおした時にイベントを走らせたい
-        // https://teratail.com/questions/243340
-        button2.addEventListener("click", () => {
-            deltask(index);
-        })
-        button1.textContent = "作業中";
-        button2.textContent = "削除";
-        td1.textContent = index;
-        td2.textContent = task.name;
+        mktaskinfo(td1,td2,index,task.name);
+        mkstatbutton(td3,task.status,index);
+        mkdelbutton(td4,index);
         output.appendChild(tr);
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
-        td3.appendChild(button1);
         tr.appendChild(td4);
-        td4.appendChild(button2);
     });
 }
